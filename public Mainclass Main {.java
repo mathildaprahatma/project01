79bfd95a6a45813da541571ca229e8cc6296dbb3
mathildaprahatma <93359590+mathildaprahatma@users.noreply.github.com>
2021-10-29 09:06:05 +0700

    Add files via upload

---
+++ public Mainclass Main {.java
@@ -0,0 +1,14 @@
+public Mainclass Main {
+    public static void main(string[] args){
+        int totalBelanja = 60000;
+        int umur = 50;
+        int beratBadan = 75;
+        if ("totalBelanja >50000 && umur >16"){
+            //kondisi true
+            system.out.println("anda mendapatkan promo");
+            {else{
+                //kondisi false
+                system.out.println("anda belum mendapatkan promo");
+                
+    
+}
