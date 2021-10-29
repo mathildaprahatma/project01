7d1bb4dc5d4f5fe40b985d93047de64c218e98ff
mathildaprahatma <93359590+mathildaprahatma@users.noreply.github.com>
2021-10-29 09:49:38 +0700

    arduino
    projek arduiono

---
+++ package uts;.js
@@ -0,0 +1,16 @@
+package uts;
+
+public class UTS1;
+    Public static void main (String[] args){
+        int totalBelanja = 60000;
+        int umur = 50;
+        int beratBadan = 75;
+        if(totalBelanja>50000 && umur >16){
+            //kondisi true
+            system.out.println("anda mendapatkan promo");
+        }else}
+        //kondisi false
+        System.out.println("anda belum mendapatkan promo");
+
+    }
+    
